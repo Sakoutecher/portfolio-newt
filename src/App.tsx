48f6ad7f4621a3ccbb85d63393ffe890472c5de2
hcampos <hcampospro@gmail.com>
2023-03-30 23:51:39 +0200

    💄 add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 //Librairies
 import React from 'react'
+import styled from 'styled-components'
 
 //Components
 import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <>
+    <MainContainer>
       <Navbar />
-    </>
+    </MainContainer>
   )
 }
+
+const MainContainer = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  background: url('./assets/background.svg');
+  background-size: cover;
+  overflow-y: scroll;
+`
 
 export default App
