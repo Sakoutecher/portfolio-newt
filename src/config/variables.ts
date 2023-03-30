59bf98407d8a30df88a5d804315ccd7ebd9f0bc4
hcampos <hcampospro@gmail.com>
2023-03-30 17:34:53 +0200

    🔧 add variables

---
+++ src/config/variables.ts
@@ -0,0 +1,22 @@
+/**Fonts */
+export const fonts = {
+  OTR: "'OTR', sans-serif",
+  WorkSans: "'Work Sans', sans-serif",
+}
+
+/**Colors */
+export const colors = {
+  textColor: '#212121',
+  secondary: '#dddddd',
+}
+
+/**Font Size */
+export const fontSize = {
+  xs: '.8em',
+  s: '.9em',
+  md: '1em',
+  lg: '1.25em',
+  xl: '1.5em',
+  xxl: '2em',
+  xxxl: '2.5em',
+}
