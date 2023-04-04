d88215eeca0a118f926839cee5c737f4eb584e0c
hcampos <hcampospro@gmail.com>
2023-04-04 15:59:38 +0200

    ✨ add projects

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Footer from './components/Footer/Footer'
 //Pages
 import Home from './pages/Home'
 import About from './pages/About'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/about'} element={<About />} />
+        <Route path={'/projects'} element={<Projects />} />
       </Routes>
       <Footer />
     </MainContainer>
