a6f962f9879f36c3fea412faeed8955da5946769
hcampos <hcampospro@gmail.com>
2023-04-04 16:17:47 +0200

    💬 projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,77 @@
+//Types
+import { ProjectType } from '../config/types'
+
+export const projects: Array<ProjectType> = [
+  {
+    title: `LIVRET, MUSEE D'ORSAY`,
+    imgPath: '',
+    mainsTag: ['GRAPHIC DESIGN'],
+    secondsTags: ['MISE EN PAGE', 'TYPOGRAPHIE'],
+  },
+  {
+    title: `AMOUR - TYPOGRAPHIE`,
+    imgPath: '',
+    mainsTag: ['GRAPHIC DESIGN'],
+    secondsTags: ['TYPOGRAPHIE'],
+  },
+  {
+    title: `HTW A & B - SPECIMEN TYPOGRAPHIE`,
+    imgPath: '',
+    mainsTag: ['GRAPHIC DESIGN'],
+    secondsTags: ['TYPOGRAPHIE', 'PRINT'],
+  },
+  {
+    title: `PERRIER X ROLLAND GARROS`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: ['ILLUSTRATION'],
+  },
+  {
+    title: `LE CAUCHEMAR CONTINUE - MENACE SANTANA`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: ['DESIGN GENERATIF'],
+  },
+  {
+    title: `AMNESIE - DAMSO`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: ['DESIGN GENERATIF'],
+  },
+  {
+    title: `BROCHURES YNOV PARIS EN REALITE AUGMENTE`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: ['ILLUSTRATION', 'REALITE AUGMENTE'],
+  },
+  {
+    title: `ANNONCE INAZUMA ELEVEN SUR DISNEY XD`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: ['ILLUSTRATION'],
+  },
+  {
+    title: `CHAMPIONNAT D'EUROPE DE LEAGUE OF LEGENDS`,
+    imgPath: '',
+    mainsTag: ['GRAPHIC DESIGN', 'MOTION DESIGN'],
+    secondsTags: [],
+  },
+  {
+    title: `CAPITOL RECORDS`,
+    imgPath: '',
+    mainsTag: ['UX UI DESIGN', 'MOTION DESIGN'],
+    secondsTags: [],
+  },
+  {
+    title: `INVERTED WORLD SERIES`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: ['ILLUSTRATION'],
+  },
+  {
+    title: `NOUVEAUX MAILLOTS, GROWECY ESPORT`,
+    imgPath: '',
+    mainsTag: ['MOTION DESIGN'],
+    secondsTags: [],
+  },
+]
