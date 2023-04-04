897fc82c85ad279f68ce4e24c28854db700b2e30
hcampos <hcampospro@gmail.com>
2023-04-04 17:27:41 +0200

    ✨ filter

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,5 @@
 //Librairies
-import React from 'react'
+import React, { useState } from 'react'
 import uuid from 'react-uuid'
 
 //Styles
@@ -17,17 +17,87 @@ import Button from '../components/Button/Button'
 import Project from '../components/Project/Project'
 
 const Projects = () => {
+  const [filter, setFilter] = useState<string>('ALL')
+
+  const setFilterALL = () => {
+    setFilter('ALL')
+  }
+
+  const setFilterMO = () => {
+    setFilter('MOTION DESIGN')
+  }
+
+  const setFilterGD = () => {
+    setFilter('GRAPHIC DESIGN')
+  }
+
+  const setFilterUX = () => {
+    setFilter('UX UI DESIGN')
+  }
+
+  const setFilterVE = () => {
+    setFilter('VIDEO EDITING')
+  }
+
+  const setFilter3D = () => {
+    setFilter('3D')
+  }
+
+  let filteredProjects = projects
+
+  if (filter === 'ALL') {
+    console.log('Filter ALL')
+    filteredProjects = projects.filter((project) => project.filter === filter)
+  } else {
+    console.log('Filter pas ALL')
+    filteredProjects = projects.filter(
+      (project) => project.mainsTag[0] === filter
+    )
+  }
+
   return (
     <ProjectsContainer>
       <FilterContainer>
-        <Button text={`MOTION DESIGN`} href={'#'} size={'s'} />
-        <Button text={`GRAPHIC DESIGN`} href={'#'} size={'s'} />
-        <Button text={`UX UI DESIGN`} href={'#'} size={'s'} />
-        <Button text={`VIDEO EDITING`} href={'#'} size={'s'} />
-        <Button text={`3D`} href={'#'} size={'s'} />
+        <Button
+          text={`TOUS`}
+          size={'s'}
+          onclick={setFilterALL}
+          active={filter === 'ALL' ? true : false}
+        />
+        <Button
+          text={`MOTION DESIGN`}
+          size={'s'}
+          onclick={setFilterMO}
+          active={filter === 'MOTION DESIGN' ? true : false}
+        />
+        <Button
+          text={`GRAPHIC DESIGN`}
+          size={'s'}
+          onclick={setFilterGD}
+          active={filter === 'GRAPHIC DESIGN' ? true : false}
+        />
+        <Button
+          text={`UX UI DESIGN`}
+          size={'s'}
+          onclick={setFilterUX}
+          active={filter === 'UX UI DESIGN' ? true : false}
+        />
+        <Button
+          text={`VIDEO EDITING`}
+          size={'s'}
+          onclick={setFilterVE}
+          active={filter === 'VIDEO EDITING' ? true : false}
+        />
+        <Button
+          text={`3D`}
+          href={'#'}
+          size={'s'}
+          onclick={setFilter3D}
+          active={filter === '3D' ? true : false}
+        />
       </FilterContainer>
       <GridContainer>
-        {projects.map(({ title, imgPath, mainsTag, secondsTags }) => (
+        {filteredProjects.map(({ title, imgPath, mainsTag, secondsTags }) => (
           <Project
             key={uuid()}
             title={title}
