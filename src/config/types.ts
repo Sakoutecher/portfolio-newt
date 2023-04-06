e3337b84d7719695a63fb35c4230d50c1873f774
hcampos <hcampospro@gmail.com>
2023-04-06 16:35:23 +0200

    🏷️ update project type

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,7 +1,23 @@
+type DescriptionType = {
+  title: string
+  desc: string
+  side: 'left' | 'right'
+}
+
 export type ProjectType = {
+  id: number
   title: string
   imgPath: string
   mainsTag: Array<string>
   secondsTags: Array<string>
   filter: string
+  imgAccueil: string
+  client: Array<string>
+  role: Array<string>
+  credits: Array<string>
+  description: Array<DescriptionType>
+  renduFinal: boolean
+  hrefButton: string
+  video: boolean
+  videoPath: string
 }
