9ce2e78850abdfa4c38ce21696c8d144a06526f9
hcampos <hcampospro@gmail.com>
2023-04-06 15:00:39 +0200

    ✨ add descriptionproject component

---
+++ src/components/DescriptionProject/DescriptionProject.tsx
@@ -0,0 +1,61 @@
+//Librairies
+import React, { FC, useState } from 'react'
+
+//Styles
+import {
+  DescriptionProjectContainer,
+  ContainerTitleDescription,
+  SideContainer,
+} from './DescriptionProject.style'
+
+//Components
+import TitleAbout from '../TitleAbout/TitleAbout'
+import DescriptionAbout from '../DescriptionAbout/DescriptionAbout'
+
+//Variables
+import { colors } from '../../config/variables'
+
+//Icons
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+
+type DescriptionProjectProps = {
+  side: 'left' | 'right'
+  children: string
+  title: string
+}
+
+const DescriptionProject: FC<DescriptionProjectProps> = ({
+  side,
+  children,
+  title,
+}) => {
+  const [hover, isHover] = useState<boolean>(false)
+
+  const handleMouseEnter = (): void => {
+    isHover(true)
+  }
+
+  const handleMouseLeave = (): void => {
+    isHover(false)
+  }
+
+  return (
+    <SideContainer side={side}>
+      <DescriptionProjectContainer>
+        <ContainerTitleDescription active={hover}>
+          <FaArrowRight className='arrowRight' color={colors.primary} />
+          <TitleAbout>{title}</TitleAbout>
+          <FaArrowLeft className='arrowLeft' color={colors.primary} />
+        </ContainerTitleDescription>
+        <DescriptionAbout
+          handleMouseEnter={handleMouseEnter}
+          handleMouseLeave={handleMouseLeave}
+        >
+          {children}
+        </DescriptionAbout>
+      </DescriptionProjectContainer>
+    </SideContainer>
+  )
+}
+
+export default DescriptionProject
