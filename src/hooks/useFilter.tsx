c6a9714b4b2e83777de90b51f75861d217b82c3d
hcampos <hcampospro@gmail.com>
2023-04-13 13:33:04 +0200

    ✨ useFilter

---
+++ src/hooks/useFilter.tsx
@@ -0,0 +1,43 @@
+//Other
+import { useState } from 'react'
+
+const useFilter = () => {
+  const [filter, setFilter] = useState<string>('ALL')
+
+  const setFilterALL = () => {
+    setFilter('ALL')
+  }
+
+  const setFilterMO = () => {
+    setFilter('MOTION DESIGN')
+  }
+
+  const setFilterGD = () => {
+    setFilter('GRAPHIC DESIGN')
+  }
+
+  const setFilterUX = () => {
+    setFilter('UX UI DESIGN')
+  }
+
+  const setFilterVE = () => {
+    setFilter('VIDEO EDITING')
+  }
+
+  const setFilter3D = () => {
+    setFilter('3D')
+  }
+
+  return {
+    filter,
+    setFilterALL,
+    setFilterMO,
+    setFilterGD,
+    setFilterUX,
+    setFilterVE,
+    setFilter3D,
+    setFilter,
+  }
+}
+
+export default useFilter
