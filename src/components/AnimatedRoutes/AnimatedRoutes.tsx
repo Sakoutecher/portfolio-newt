3dec3eda65993dc1238dcc9d78643a9771d4bf5f
hcampos <hcampospro@gmail.com>
2023-04-14 10:12:53 +0200

    ✨ animated routes component

---
+++ src/components/AnimatedRoutes/AnimatedRoutes.tsx
@@ -0,0 +1,27 @@
+//Librairies
+import React from 'react'
+import { AnimatePresence } from 'framer-motion'
+import { useLocation, Routes, Route } from 'react-router-dom'
+
+//Pages
+import Home from '../../pages/Home'
+import About from '../../pages/About'
+import Projects from '../../pages/Projects'
+import OneProject from '../../pages/OneProject'
+
+const AnimatedRoutes = () => {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/about'} element={<About />} />
+        <Route path={'/projects'} element={<Projects />} />
+        <Route path={'/one-project/:id'} element={<OneProject />} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
+export default AnimatedRoutes
