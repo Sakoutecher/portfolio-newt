7460080ace9d692fd9f44cc6c27db71b4ee8533d
hcampos <hcampospro@gmail.com>
2023-04-17 14:42:20 +0200

    📱 responsive phone

--- src/components/Credits/Credits.style.ts
+++ src/components/Credits/Credits.style.ts
@@ -2,7 +2,7 @@
 import styled from 'styled-components'
 
 //Variables
-import { fontSize, colors, fonts } from '../../config/variables'
+import { fontSize, colors, fonts, mediaSize } from '../../config/variables'
 
 export const CreditsBlock = styled.div`
   display: flex;
@@ -15,4 +15,8 @@ export const Text = styled.span`
   font-family: ${fonts.OTR};
   color: ${colors.primary};
   font-size: ${fontSize.xs};
+
+  @media ${mediaSize.phone} {
+    font-size: 0.1em;
+  }
 `
