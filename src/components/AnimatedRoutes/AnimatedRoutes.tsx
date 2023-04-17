dbb0d6eb5de7df06cf33d094180243d94a48a187
hcampos <hcampospro@gmail.com>
2023-04-17 09:40:48 +0200

    🐛 enter and exit transition

--- src/components/AnimatedRoutes/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes/AnimatedRoutes.tsx
@@ -1,7 +1,7 @@
 //Librairies
 import React from 'react'
 import { AnimatePresence } from 'framer-motion'
-import { useLocation, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 //Pages
 import Home from '../../pages/Home'
@@ -10,11 +10,9 @@ import Projects from '../../pages/Projects'
 import OneProject from '../../pages/OneProject'
 
 const AnimatedRoutes = () => {
-  const location = useLocation()
-
   return (
-    <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
+    <AnimatePresence mode='wait'>
+      <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/about'} element={<About />} />
         <Route path={'/projects'} element={<Projects />} />
--- src/components/PageTransition/PageTransition.tsx
+++ src/components/PageTransition/PageTransition.tsx
@@ -1,14 +1,18 @@
 //Librairies
 import React, { FC, ReactNode } from 'react'
 import { motion } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
 
 type PageTransitionProps = {
   children: ReactNode
 }
 
 const PageTransition: FC<PageTransitionProps> = ({ children }) => {
+  const location = useLocation()
+
   return (
     <motion.div
+      key={location.pathname}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
