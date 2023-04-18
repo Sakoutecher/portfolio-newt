72fe10b38d413905bed1bb5ab5c641d40f32d566
hcampos <hcampospro@gmail.com>
2023-04-18 11:33:40 +0200

    ✅ button test types

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { describe, it } from 'vitest'
+import { describe, it, expectTypeOf } from 'vitest'
 import Button from './Button'
 
+type ButtonProps = {
+  text: string
+  href?: string
+  size: 's' | 'md' | 'lg'
+  style?: object
+  active?: boolean
+  onclick?: () => void
+}
+
 describe('Button', () => {
+  it('button should be a function', () => {
+    expectTypeOf(Button).toBeFunction()
+  })
+
+  it('button props types are all good', () => {
+    const props: ButtonProps = {
+      text: 'About',
+      href: '/about',
+      size: 'md',
+      style: { display: 'flex' },
+      active: true,
+      onclick: () => {
+        console.log('Button')
+      },
+    }
+    expect(typeof props.text).toBe('string')
+    expect(typeof props.href).toBe('string')
+    expect(typeof props.size).toBe('string')
+    expect(typeof props.style).toBe('object')
+    expect(typeof props.active).toBe('boolean')
+    expect(typeof props.onclick).toBe('function')
+  })
+
   it('should display a button with an href and a text inside', () => {
     render(<Button text='About' size='md' href='/about' />)
     const button = screen.getByText('About')
