1baab921ba41a977e335608772cbc7ddea212e04
hcampos <hcampospro@gmail.com>
2023-04-19 11:14:31 +0200

    🚚 resolve path

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { describe, it, expectTypeOf } from 'vitest'
-import Button from './Button'
+import { Button } from './Button'
 
 type ButtonProps = {
   text: string
