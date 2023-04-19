1b0d350c20b1d3e532a180ab04933a27da25a64d
hcampos <hcampospro@gmail.com>
2023-04-19 10:44:56 +0200

    🏷️ rename props types

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -4,14 +4,14 @@ import React, { FC } from 'react'
 //Styles
 import { GlobalTag } from './Tag.style'
 
-type Props = {
+type TagProps = {
   text: string
   size: 's' | 'md' | 'lg'
   style?: object
   main: boolean
 }
 
-const Tag: FC<Props> = ({ text, size, style, main }) => {
+const Tag: FC<TagProps> = ({ text, size, style, main }) => {
   return (
     <GlobalTag style={style} size={size} href={'#'} main={main}>
       {text}
