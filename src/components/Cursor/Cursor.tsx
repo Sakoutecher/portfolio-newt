6d15ef04c33a3f190db56602bfd964af0405bbbe
hcampos <hcampospro@gmail.com>
2023-04-19 14:30:48 +0200

    🐛 center cursor with user cursor

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -13,7 +13,7 @@ export const Cursor = () => {
     window.addEventListener('mousemove', (e) => {
       if (cursorRef.current)
         cursorRef.current.style.transform = `translate3d(${
-          e.clientX - 735
+          e.clientX - window.innerWidth / 2
         }px, ${e.clientY}px, 0)`
 
       if (e.target && (e.target as HTMLElement).getAttribute('data-cursor')) {
