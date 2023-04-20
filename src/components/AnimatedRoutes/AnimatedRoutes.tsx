461d9230cac25c743c67a533c270353727354ba5
hcampos <hcampospro@gmail.com>
2023-04-20 14:11:51 +0200

    ✨ contact page

--- src/components/AnimatedRoutes/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes/AnimatedRoutes.tsx
@@ -8,6 +8,7 @@ import { Home } from '../../pages/Home'
 import { About } from '../../pages/About'
 import { Projects } from '../../pages/Projects'
 import { OneProject } from '../../pages/OneProject'
+import { Contact } from '../../pages/Contact'
 
 export const AnimatedRoutes = () => {
   return (
@@ -17,6 +18,7 @@ export const AnimatedRoutes = () => {
         <Route path={'/about'} element={<About />} />
         <Route path={'/projects'} element={<Projects />} />
         <Route path={'/one-project/:id'} element={<OneProject />} />
+        <Route path={'/contact'} element={<Contact />} />
       </Routes>
     </AnimatePresence>
   )
