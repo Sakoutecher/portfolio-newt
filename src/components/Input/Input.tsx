cfe5c5662b1819fd7c09394ad6911d89b7a2d208
hcampos <hcampospro@gmail.com>
2023-04-20 14:09:55 +0200

    ✨ add input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,36 @@
+//Librairies
+import React, { FC, useState } from 'react'
+
+//Styles
+import { InputContainer, InputElement } from './Input.style'
+
+type InputProps = {
+  name?: string
+  type: 'password' | 'text' | 'email'
+  id?: string
+  placeholder: string
+}
+
+export const Input: FC<InputProps> = ({ name, type, id, placeholder }) => {
+  const [isFocused, setIsFocused] = useState(false)
+
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
+
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
+
+  return (
+    <InputContainer focused={isFocused} placeholder={placeholder}>
+      <InputElement
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        name={name}
+        type={type}
+        id={id}
+      />
+    </InputContainer>
+  )
+}
