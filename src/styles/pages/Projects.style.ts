7462f2f6d890f2c7563b0d75ec606b59eaa704bf
hcampos <hcampospro@gmail.com>
2023-04-25 11:48:38 +0200

    🐛 fix phone responsive grid project

--- src/styles/pages/Projects.style.ts
+++ src/styles/pages/Projects.style.ts
@@ -32,7 +32,7 @@ export const FilterContainer = styled.div`
 export const GridContainer = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 15px;
   grid-auto-rows: 400px;
   margin-bottom: 3%;
